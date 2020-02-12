d5492c4ad97bea3a7377e1889a30c2050d84a5f7
Niel Schrage <nschrage@college.harvard.edu>
2020-02-12 14:22:57 -0500

    histogram commit

--- seat_clumping.R
+++ seat_clumping.R
@@ -1,3 +1,7 @@
+library(tidyverse)
+library(reprex)
+library(ggplot2)
+
 vector<- (1:30)
 genders<- c(rep("M",14),rep("F",16))
 x<-data.frame(vector, genders, sample(1:30,30,replace=F))
@@ -5,7 +9,50 @@ colnames(x) <- c("student", "genders", "seat")
 
 
 x<-x %>%
-  mutate(table = floor(seat/4 + 1)) %>%
+  mutate(table = ceiling(seat/4)) %>%
   group_by(table) %>%
   count(genders) %>%
-  filter()
+  filter(table != 8) %>% 
+  filter(genders == "M" & n == 2) %>% 
+  nrow()
+
+n_sims <- 1000
+n_tables <- rep(NA, n_sims)
+
+for (s in 1:n_sims){
+  
+  vector<- (1:30)
+  genders<- c(rep("M",14),rep("F",16))
+  x<-data.frame(vector, genders, sample(1:30,30,replace=F))
+  colnames(x) <- c("student", "genders", "seat")
+
+  x<-x %>%
+    mutate(table = ceiling(seat/4)) %>%
+    group_by(table) %>%
+    count(genders) %>%
+    filter(table != 8) %>% 
+    filter(genders == "M" & n == 2) %>% 
+    nrow()
+  
+    n_tables[s] = x
+}
+
+hist(n_tables)
+
+n_tables_graph = as.data.frame(n_tables)
+
+n_tables_graph %>% 
+  ggplot() + 
+  geom_histogram(aes(x = n_tables), bins = 8) + 
+  labs(title = "Simulated Distribution of Even Tables",
+       subtitle = "Red = Simulated Mean, Blue = Observed Mean",
+       caption = "David Kane, Gov 1006, February 12") +
+  
+  geom_vline(aes(xintercept = mean(n_tables)), color = "red", linetype = "dashed", size = .5) +
+               
+  geom_vline(aes(xintercept = 2), color = "blue", linetype = "dashed", size = .5)
+
+
+
+
+
